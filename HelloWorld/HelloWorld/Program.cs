c0eaeb6debbbf7353c82e683e5dc649037f60a3c
John Dawkins <66180847+jtylerdawkins@users.noreply.github.com>
2023-08-06 17:07:10 -0700

    First commit
    Initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "World";
+
+// Print "Hello, name!"
+Console.WriteLine($"Hello, {name}!");
+
+//Console.WriteLine("Hello, World!");
+// test
