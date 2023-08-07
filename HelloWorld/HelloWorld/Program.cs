22414ee0d24dcafdab626afdbfa273f85d70413a
John Dawkins <66180847+jtylerdawkins@users.noreply.github.com>
2023-08-06 17:49:14 -0700

    Rock Paper Scissors
    Added some rock paper scissors gameplay for fun.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -5,4 +5,82 @@ string name = "World";
 Console.WriteLine($"Hello, {name}!");
 
 //Console.WriteLine("Hello, World!");
-// test
+
+// Rock, paper, scissors mini-game
+bool isPlaying = true;
+int score = 0;
+while (isPlaying)
+{
+    // Begin playing rock, paper, scissors
+    Console.WriteLine("Let's play: [r]ock, [p]aper, [s]cissors.");
+    string input = Console.ReadLine().ToLower();
+    // Validate the user's input
+    while (input != "r" && input != "p" && input != "s")
+    {
+        Console.WriteLine("Invalid input. Please try again.");
+        input = Console.ReadLine().ToLower();
+    }
+
+    // Generate a random number between 0 and 2 for the computer's choice
+    Random rand = new Random();
+    int computerChoice = rand.Next(0, 3);
+    string computerChoiceString = "";
+    // 0 = rock, 1 = paper, 2 = scissors
+    if (computerChoice == 0)
+    {
+        computerChoiceString = "rock";
+    }
+    else if (computerChoice == 1)
+    {
+        computerChoiceString = "paper";
+    }
+    else
+    {
+        computerChoiceString = "scissors";
+    }
+    Console.WriteLine($"Computer chose {computerChoiceString}");
+
+    // Compare the user's input to the computer's choice
+    if (input == computerChoiceString)
+    {
+        Console.WriteLine("It's a tie!");
+    }
+    else if (input == "r" && computerChoiceString == "scissors")
+    {
+        Console.WriteLine("You win!");
+        score++;
+    }
+    else if (input == "p" && computerChoiceString == "rock")
+    {
+        Console.WriteLine("You win!");
+        score++;
+    }
+    else if (input == "s" && computerChoiceString == "paper")
+    {
+        Console.WriteLine("You win!");
+        score++;
+    }
+    else
+    {
+        Console.WriteLine("You lose!");
+    }
+
+    // Ask the user if they want to play again
+    Console.WriteLine("Play again? (y/n)");
+    string playAgain = Console.ReadLine();
+    while (playAgain != "y" && playAgain != "n")
+    {
+        Console.WriteLine("Invalid input. Please enter 'y' or 'n' for yes or no, respectively.");
+        playAgain = Console.ReadLine();
+    }
+
+    if (playAgain == "n")
+    {
+        Console.WriteLine($"Final score: {score}");
+        isPlaying = false;
+    }
+    else
+    {
+        Console.WriteLine($"Score: {score}");
+    }
+}
